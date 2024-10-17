3bf38b0724f9579d09b65d8679ecc1174adf414b
BB223 <79644542+BB223@users.noreply.github.com>
2024-10-17 19:03:45 +0200

    clippy

--- task4/src/main.rs
+++ task4/src/main.rs
@@ -7,20 +7,18 @@ use std::{
 
 fn main() {
     if let Ok(lines) = read_lines("./4_input.txt") {
-        let mut iter = lines.map_while(Result::ok);
-        while let Some(line) = iter.next() {
+        let iter = lines.map_while(Result::ok);
+        for line in iter {
             let input: HashSet<u32> = line.split(" ").map(|s| s.parse::<u32>().unwrap()).collect();
 
             let min = input.iter().min().unwrap();
-            let min = min.clone();
             let max = input.iter().max().unwrap();
-            let max = max.clone();
             let missing = max + 1 - min - input.len() as u32;
             print!("{}: ", missing);
 
             let mut missing = Vec::new();
 
-            for val in min + 1..max {
+            for val in min + 1..*max {
                 if !input.contains(&val) {
                     missing.push(val)
                 }
